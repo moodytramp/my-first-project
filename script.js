0db77e24d61a802e525f0c3171831c162d2450cf
Yassine <yassine.jai@icloud.com>
2026-05-19 20:41:23 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("myButton")
+
+button.addEventListener("click", function() {
+  button.textContent = "You clicked me!"
+  button.style.background = "purple"
+})
